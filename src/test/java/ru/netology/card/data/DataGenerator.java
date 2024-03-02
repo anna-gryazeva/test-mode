eebe9cb6aa10f4ad98a6c65e396f102b1918ff13
Anna Gryazeva <anna_gryazeva@mail.ru>
2024-03-02 22:57:13 +0300

    Initial commit

---
+++ src/test/java/ru/netology/card/data/DataGenerator.java
@@ -0,0 +1,21 @@
+package ru.netology.card.data;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.Locale;
+
+public class DataGenerator {
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    private static final Faker faker = new Faker(new Locale("en"));
+
+}
