1f880708d75e73c1a8c3cd76e86e3bf89cff3aa3
Anna Gryazeva <anna_gryazeva@mail.ru>
2024-03-04 22:43:36 +0300

    add tests

--- src/test/java/ru/netology/card/data/DataGenerator.java
+++ src/test/java/ru/netology/card/data/DataGenerator.java
@@ -5,8 +5,11 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import lombok.Value;
 
 import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
 
 public class DataGenerator {
     private static final RequestSpecification requestSpec = new RequestSpecBuilder()
@@ -17,5 +20,47 @@ public class DataGenerator {
             .log(LogDetail.ALL)
             .build();
     private static final Faker faker = new Faker(new Locale("en"));
+
+    private DataGenerator() {
+    }
+
+    private static RegistrationDto sendRequest(RegistrationDto user) {
+        given()
+                .spec(requestSpec)
+                .body(user)
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+        return user;
+    }
+
+    public static String getRandomLogin() {
+        return faker.name().username();
+    }
+
+    public static String getRandomPassword() {
+        return faker.internet().password();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationDto getUser(String status) {
+            return new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+        }
+
+        public static RegistrationDto getRegisteredUser(String status) {
+            return sendRequest(getUser(status));
+        }
+    }
+
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
 
 }
--- src/test/java/ru/netology/card/test/AuthTest.java
+++ src/test/java/ru/netology/card/test/AuthTest.java
@@ -1,4 +1,84 @@
 package ru.netology.card.test;
 
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.card.data.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.card.data.DataGenerator.Registration.getUser;
+import static ru.netology.card.data.DataGenerator.getRandomLogin;
+import static ru.netology.card.data.DataGenerator.getRandomPassword;
+
+
 public class AuthTest {
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        $("[name='login']").setValue(registeredUser.getLogin());
+        $("[name='password']").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("h2").shouldHave(Condition.exactText("Личный кабинет")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = getUser("active");
+        $("[name='login']").setValue(notRegisteredUser.getLogin());
+        $("[name='password']").setValue(notRegisteredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10))
+                .shouldBe((Condition.visible));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        var blockedUser = getRegisteredUser("blocked");
+        $("[name='login']").setValue(blockedUser.getLogin());
+        $("[name='password']").setValue(blockedUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Пользователь заблокирован"), Duration.ofSeconds(10))
+                .shouldBe((Condition.visible));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login whith wrong")
+    void shouldGetErrorIfLoginWrong() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = getRandomLogin();
+        $("[name='login']").setValue(wrongLogin);
+        $("[name='password']").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10))
+                .shouldBe((Condition.visible));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = getRandomPassword();
+        $("[name='login']").setValue(registeredUser.getLogin());
+        $("[name='password']").setValue(wrongPassword);
+        $("button.button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10))
+                .shouldBe((Condition.visible));
+    }
 }
